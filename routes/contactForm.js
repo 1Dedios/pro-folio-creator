a94d79c386881ed0127e32914999e37d17dec558
Dedios <dediosprogramming@gmail.com>
2025-12-06 00:32:18 -0500

    feat: contact-form route for user portfolios

--- routes/contactForm.js
+++ routes/contactForm.js
@@ -1,113 +1,95 @@
 import { Router } from "express";
-import { ObjectId } from "mongodb";
-import { messages, users } from "../config/mongoCollections.js";
-import { closeConnection } from "../config/mongoConnection.js";
-import { validateString, validateEmail } from "../helpers.js";
+import { createMessage } from "../data/messages.js";
+import { getPortfolioById } from "../data/portfolios.js";
+import { getUserById } from "../data/users.js";
+import sendEmail from "../services/nodemailer/email.js";
+import { validateString, validateEmail, escapeHTML } from "../helpers.js";
 
 const router = Router();
 
 router
-  .route("/")
+  .route("/:portfolioId")
   .get(async (req, res) => {
     try {
-      res.render("contact-form", { pageTitle: "Contact Form" });
+      const portfolioId = req.params.portfolioId;
+
+      return res.render("contact-form", {
+        pageTitle: "Contact Form",
+        portfolioId: portfolioId,
+      });
     } catch (e) {
-      return res.sendStatus(500);
+      return res.sendStatus(500).json({ error: e.message });
     }
   })
   .post(async (req, res) => {
-    const reqURL = req.url;
-    const name = req.body.senderName;
-    const email = req.body.emailRecipient;
-    const message = req.body.message;
-    console.table({ reqURL, name, email, message });
+    const { senderName, senderEmail, message } = req.body;
+    const validationErrors = {};
     let validatedName;
     let validatedEmail;
     let validatedMessage;
 
+    // xss mitigation
+    let fieldsArr = [senderName, senderEmail, message];
+    fieldsArr = escapeHTML(fieldsArr);
+
     try {
-      // STATUS: validating the 3 fields of the form
-      try {
-        validatedName = validateString(name);
-      } catch (e) {
-        console.warn("ATTEMPTING TO VALIDATE NAME FIELD...");
-        console.log(e.message);
-        return res.sendStatus(500);
-      }
+      validatedName = validateString(fieldsArr[0]);
+    } catch (e) {
+      validationErrors.name = e.message;
+    }
+
+    try {
+      validatedEmail = validateEmail(fieldsArr[1]);
+    } catch (e) {
+      validationErrors.email = e.message;
+    }
+
+    try {
+      validatedMessage = validateString(fieldsArr[2]);
+    } catch (e) {
+      validationErrors.message = e.message;
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      return res.status(400).json({ error: validationErrors });
+    }
+
+    try {
+      let userEmail;
+      const { portfolioId } = req.params;
 
       try {
-        validatedEmail = validateEmail(email);
-      } catch (e) {
-        console.warn("ATTEMPTING TO VALIDATE EMAIL FIELD...");
-        console.log(e.message);
-        return res.sendStatus(500);
-      }
+        const getPortfolio = await getPortfolioById(portfolioId);
+        const getUser = await getUserById(getPortfolio.ownerId);
+        userEmail = getUser.email;
 
-      try {
-        // TODO: add xss replace script you have here
-        validatedMessage = validateString(message);
-      } catch (e) {
-        console.warn("ATTEMPTING TO VALIDATE MESSAGE FIELD...");
-        console.log(e.message);
-        return res.sendStatus(500);
-      }
-
-      // TODO: 2 things must happen:  1) store contact-form contents in messages collection 2) send message to recipient
-      try {
-        try {
-          // STATUS: 1) store contact-form contents in messages collection
-          // STATUS: vars to info:
-          // validatedName
-          // validatedEmail
-          // validatedMessage
-          // STATUS: fields in messages collection:
-          // _id
-          // portfolioId - new ObjectId instance with portfolio._id
-          // senderName - validatedName
-          // senderEmail - validatedEmail
-          // message - validatedMessage
-          // sentAt - ISODate
-
-          // TODO: need to query db for recipientEmail to get their id thus we associate user with message for them
-          const usersCollection = await users();
-          const findUser = await usersCollection.find({ email: validateEmail });
-          console.table("USER Query", findUser);
-          // STATUS: so below should be the proposed messages collection schema
-          const newMessageDoc = {
-            recipientId: new ObjectId(findUser._id),
-            senderName: validatedName,
-            message: validatedMessage,
-          };
-          const messagesCollection = await messages();
-          const insertOp = await messagesCollection.insertOne(newMessageDoc);
-
-          if (!insertOp.insertedId) {
-            throw new Error("Error: Unable to add message to db.");
-          } else {
-            console.log(
-              "SUCCESSFULLY ADDED MESSAGE TO COLLECTION WITH ID: ",
-              insertOp.insertedId
-            );
-          }
-        } catch (e) {
-          console.warn(e.message);
-          return res.status(500).json({ error: e.message });
-        }
-        // STATUS: 2) sending message to recipient - the main work of this try...catch block
-        // TODO: implement nodemailer
+        // Message insertion into DB
+        await createMessage(
+          portfolioId,
+          validatedName,
+          validatedEmail,
+          validatedMessage
+        );
       } catch (e) {
         console.warn(e.message);
         return res.status(500).json({ error: e.message });
       }
-      // TODO: render a success page and then redirect them to previous page - which should be portfolio they came from
-      return res.render("success", { redirectUrl: reqURL, delay: 3000 });
+
+      await sendEmail(
+        userEmail,
+        validatedName,
+        validatedEmail,
+        validatedMessage
+      );
+
+      return res.render("success", {
+        senderName: validatedName,
+        portfolioId: portfolioId,
+        delay: 3000,
+      });
     } catch (e) {
-      console.error(e.message);
+      console.warn(e.message);
       return res.status(500).json({ error: e.message });
-    } finally {
-      // TODO: close connection to db
-      console.log("CLOSING DB CONNECTION ");
-      await closeConnection();
     }
   });
 
--- services/nodemailer/email.js
+++ services/nodemailer/email.js
@@ -1,5 +1,33 @@
 import nodemailer from "nodemailer";
 
+export default async function sendEmail(
+  userEmail,
+  senderName,
+  senderEmail,
+  message
+) {
+  try {
+    const transporterTest = await transporter.verify();
+    if (!transporterTest)
+      throw new Error("Nodemailer transporter verification failure");
+
+    // returns info object with message ID, if necessary
+    await transporter.sendMail({
+      from: `"Pro-folio" <${senderEmail}>`,
+      to: `${userEmail}`,
+      cc: `${process.env.PROFOLIO}`,
+      bcc: `${process.env.USERNAME}`,
+      replyTo: `${senderEmail}`,
+      // Note: can extend to include portfolio name or portfolio id from which message was sent
+      subject: `New Message from ${senderName} about one of your Pro-folios!`,
+      text: `${message}`,
+      html: `<p>${message}</p>`,
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -10,43 +38,3 @@ const transporter = nodemailer.createTransport({
     refreshToken: process.env.PROFOLIO_REFRESH_TOKEN,
   },
 });
-
-const transporterTest = await transporter.verify();
-console.log(`Transporter Verification: ${transporterTest}`);
-if (!transporterTest)
-  throw new Error("Nodemailer transporter verification failure");
-
-export const sendEmail = async (
-  userEmail,
-  senderName,
-  senderEmail,
-  message
-) => {
-  try {
-    await transporter.sendMail({
-      from: `"Pro-folio" <${senderEmail}>`,
-      to: `${userEmail}`,
-      cc: `${process.env.PROFOLIO}`,
-      bcc: `${process.env.USERNAME}, xatrumzw@sharklasers.com`,
-      replyTo: `${senderEmail}`,
-      // Note: can extend to include portfolio name or portfolio id from which message was sent
-      subject: `New Message from ${senderName} about one of your Pro-folios!`,
-      text: `${message}`,
-      html: `<p>${message}</p>`,
-    });
-  } catch (error) {
-    throw error;
-  }
-};
-
-/* 
-// old callback in sendMail as second arg
-(err, info) => {
-  if (err) {
-    console.error("SEND MAIL ERROR: ", err);
-    throw new Error(`SEND MAIL ERROR: ${err}`);
-  }
-  // STATUS: otherwise print info ? what to do with this besides verify?
-  console.log(`SEND MAIL SUCCESS: ENVELOPE - ${info.envelope}`);
-  console.log(`SEND MAIL SUCCESS: ID - ${info.messageId}`);
-}; */
